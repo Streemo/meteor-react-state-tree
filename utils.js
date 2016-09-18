bd35ed6ea269226af3e20d994ce84ce41562e0f2
Jay Patel <jay@pitel.co>
2016-09-18 18:34:11 -0400

    spaces

--- utils.js
+++ utils.js
@@ -1,16 +1,16 @@
 export const isTree = function(obj){
-	return typeof obj === "object" && !!obj;
+  return typeof obj === "object" && !!obj;
 }
 // proj: N => S, takes a nested tree and maps it to a non-nested tree.
 // Idempotency; proj: S => S is I, the identity.
 export const proj = function(tree,val,isCombinatoric,root="",reduced={}){
-	if (isTree(tree)){
-		for (let k in tree){
+  if (isTree(tree)){
+    for (let k in tree){
       const r = root+(root&&".")+k;
       isCombinatoric && (reduced[r] = tree[k])
-			proj(tree[k],val,isCombinatoric,r,reduced)
+      proj(tree[k],val,isCombinatoric,r,reduced)
     }
-		return reduced;
-	}
-	reduced[root] = val || tree;
+    return reduced;
+  }
+  reduced[root] = val || tree;
 }
